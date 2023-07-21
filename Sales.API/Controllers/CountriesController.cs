4f7c44577fbff661fa3fa75d4980e8edbbbcec48
snieves74 <snieves74@hotmail.com>
2023-07-21 17:24:25 -0500

    Complete countries API controles

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -22,6 +22,17 @@ namespace Sales.API.Controllers
             return Ok(await _context.Countries.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country =await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
@@ -29,5 +40,28 @@ namespace Sales.API.Controllers
             await _context.SaveChangesAsync();
             return Ok(country);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
     }
 }
